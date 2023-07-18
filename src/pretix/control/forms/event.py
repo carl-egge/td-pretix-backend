5192559d23cfbf30995ef38169ade39ce01879fd
Carl Egge <carl.egge@web.de>
2023-07-18 10:25:04 +0200

    Add description to Event

--- src/pretix/control/forms/event.py
+++ src/pretix/control/forms/event.py
@@ -169,6 +169,7 @@ class EventWizardBasicsForm(I18nModelForm):
         fields = [
             'name',
             'slug',
+            'desc',
             'currency',
             'date_from',
             'date_to',
@@ -440,6 +441,7 @@ class EventUpdateForm(I18nModelForm):
         fields = [
             'name',
             'slug',
+            'desc',
             'currency',
             'date_from',
             'date_to',
