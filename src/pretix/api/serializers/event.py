173f13e9e1e5912c7b8570a0c9affcf49b45d610
Carl Egge <carl.egge@web.de>
2024-02-08 20:38:28 +0100

    Add subtitle to Event

--- src/pretix/api/serializers/event.py
+++ src/pretix/api/serializers/event.py
@@ -194,7 +194,7 @@ class EventSerializer(SalesChannelMigrationMixin, I18nAwareModelSerializer):
 
     class Meta:
         model = Event
-        fields = ('name', 'slug', 'desc', 'picture', 'live', 'testmode', 'currency', 'date_from',
+        fields = ('name', 'slug', 'desc', 'subtitle', 'picture', 'live', 'testmode', 'currency', 'date_from',
                   'date_to', 'date_admission', 'is_public', 'presale_start',
                   'presale_end', 'location', 'geo_lat', 'geo_lon', 'has_subevents', 'meta_data', 'seating_plan',
                   'plugins', 'seat_category_mapping', 'timezone', 'item_meta_properties', 'valid_keys',
